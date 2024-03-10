9caa8f50a506b79531247a17f0dbdd00026e6ac3
Prism182 <kylefos@icloud.com>
2024-03-10 07:58:16 +0000

    Added new block called Raw sapphire

--- src/main/java/net/prism182/tutorialmod/Block/ModBlocks.java
+++ src/main/java/net/prism182/tutorialmod/Block/ModBlocks.java
@@ -21,6 +21,8 @@ public class ModBlocks {
 
     public static final RegistryObject<Block> SAPPHIRE_BLOCK = registerBlock("sapphire_block",
             () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.AMETHYST)));
+    public static final RegistryObject<Block> RAW_SAPPHIRE_BLOCK = registerBlock("raw_sapphire_block",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).sound(SoundType.BONE_BLOCK)));
 
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
--- src/main/java/net/prism182/tutorialmod/item/ModCreativeModTabs.java
+++ src/main/java/net/prism182/tutorialmod/item/ModCreativeModTabs.java
@@ -25,6 +25,7 @@ public class ModCreativeModTabs {
                         output.accept(Items.DIAMOND);
 
                         output.accept(ModBlocks.SAPPHIRE_BLOCK.get());
+                        output.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
 
                     })
                     .build());
