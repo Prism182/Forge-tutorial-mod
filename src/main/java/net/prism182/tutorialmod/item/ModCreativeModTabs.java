197bca01f1a5deb13ab5710dbd42cfc6d8421b4c
Prism182 <kylefos@icloud.com>
2024-03-10 07:44:06 +0000

    Cleaned up code

--- src/main/java/net/prism182/tutorialmod/item/ModCreativeModTabs.java
+++ src/main/java/net/prism182/tutorialmod/item/ModCreativeModTabs.java
@@ -3,7 +3,6 @@ package net.prism182.tutorialmod.item;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraftforge.eventbus.api.IEventBus;
